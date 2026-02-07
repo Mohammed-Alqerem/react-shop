670388da6a92f334645ce2a8b3e31a00c7419d6e
mohammed alqerem <mahammedalqerem422@gmail.com>
2026-02-07 09:50:16 +0200

    init project SPA

---
+++ src/routers.jsx
@@ -0,0 +1,42 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "./layouts/MainLayout";
+import Home from "./pages/home/Home";
+import Cart from "./pages/cart/Cart";
+import Register from "./pages/auth/register/Register";
+import Login from "./pages/auth/login/Login";
+
+
+
+const router = createBrowserRouter([
+
+
+    {
+        path:'/',
+        element:<MainLayout/>,
+        children:[
+            {
+                index:true,
+                element:<Home/>
+            },
+            {
+
+                path:'cart',
+                element:<Cart/>
+            },
+            {
+                path:'register',
+                element:<Register/>
+            },
+            {
+                path:'login',
+                element:<Login/>
+            }
+        ]
+    }
+
+
+
+]);
+
+
+export default router;
