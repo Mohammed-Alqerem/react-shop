a4fe016a616dd2829a9096445c87ac908d89bd72
mohammed alqerem <mahammedalqerem422@gmail.com>
2026-02-08 18:55:29 +0200

    edit the TextField closing tag

--- src/pages/auth/register/Register.jsx
+++ src/pages/auth/register/Register.jsx
@@ -32,25 +32,18 @@ export default function Register() {
       </Typography>
 
       <Box onSubmit={handleSubmit(handleRegister)} component={'form'} display={'flex'} flexDirection={'column'} gap={3} alignItems={'center'}>
-        <TextField {...register('userName')} fullWidth label='User Name' variant='outlined'
-        error={errors.userName}
-        helperText={errors.userName?.message}></TextField>
-
-        <TextField {...register('fullName')}fullWidth label='Full Name' variant='outlined'
-         error={errors.fullName}
-         helperText={errors.fullName?.message}></TextField>
-
-        <TextField {...register('email')}  fullWidth label='Email' variant='outlined'
-        error={errors.email}
-        helperText={errors.email?.message}></TextField>
-
-        <TextField {...register('password')} fullWidth label='Password' variant='outlined'
-        error={errors.password}
-        helperText={errors.password?.message}></TextField>
-
-        <TextField {...register('phoneNumber')}fullWidth label='Phone Number' variant='outlined'
-        error={errors.phoneNumber} helperText={errors.phoneNumber?.message}></TextField>
-
+        <TextField {...register('userName')} fullWidth label="User Name" variant="outlined"
+           error={errors.userName} helperText={errors.userName?.message} />
+          <TextField {...register('fullName')} fullWidth label="FullName" variant="outlined"
+          error={errors.fullName} helperText={errors.fullName?.message} />
+          <TextField {...register('email')} fullWidth label="Email" variant="outlined"
+           error={errors.email} helperText={errors.email?.message} />
+          <TextField {...register('password')} fullWidth label="Password" variant="outlined"
+           error={errors.password} helperText={errors.password?.message}
+           />
+          <TextField {...register('phoneNumber')} fullWidth label="Phone Number" variant="outlined"
+          error={errors.phoneNumber } helperText={errors.phoneNumber?.message}
+            />
         <Button type='submit' variant='outlined' >Register</Button>
       </Box>
     </Box>
