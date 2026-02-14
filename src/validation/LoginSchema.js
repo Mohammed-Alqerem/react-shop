4b5348a35e3d0ed00c9c104960072fcc0e35275f
mohammed alqerem <mahammedalqerem422@gmail.com>
2026-02-14 18:52:17 +0200

    enhance the validation message of the form to login and register

--- src/validation/LoginSchema.js
+++ src/validation/LoginSchema.js
@@ -3,10 +3,10 @@ import * as yup from "yup"
             email:yup.string().email("email must be  a valid email").required("email is required"),
             password:yup.string().required("password is required")
             .min(6,"password must be at least 6 characters")
-            .matches(/[A-Z]/)
-            .matches(/[a-z]/)
-            .matches(/[0-9]/)
-            .matches(/[$@&*?!]/)
+            .matches(/[A-Z]/,"password must contain at least one uppercase letter")
+            .matches(/[a-z]/,"password must contain at least one lowercase letter")
+            .matches(/[0-9]/,"password must contain at least one number")
+            .matches(/[$@&*?!]/,"password must contain at least one special character")
     });
 
 
--- src/validation/RegisterSchema.js
+++ src/validation/RegisterSchema.js
@@ -2,16 +2,16 @@ import * as yup from "yup"
 
   export const registerSchema = yup.object({
 
-    userName:yup.string().required("userName is required").min(3,"userName must be at least 3 charachters")
+    userName:yup.string().required("userName is required").min(3,"userName must be at least 3 characters")
     .matches(/[a-zA-Z0-9._-]+$/,"userName must contain only letters, numbers, underscores, and hyphens"),
     fullName:yup.string().required("fullName is required"),
     email:yup.string().email("email must be  a valid email").required("email is required"),
     password:yup.string().required("password is required")
-    .min(6,"password must be at least 6 charachters")
-    .matches(/[A-Z]/)
-    .matches(/[a-z]/)
-    .matches(/[0-9]/)
-    .matches(/[$@&*?!]/),
+    .min(6,"password must be at least 6 characters")
+    .matches(/[A-Z]/,"password must contain at least one uppercase letter")
+    .matches(/[a-z]/,"password must contain at least one lowercase letter")
+    .matches(/[0-9]/,"password must contain at least one number")
+    .matches(/[$@&*?!]/,"password must contain at least one special character"),
     phoneNumber:yup.string().required("phoneNumber is required")
    
   });
